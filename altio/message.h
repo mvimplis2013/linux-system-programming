54459a3169678110faf111c48da089d49cffa15f
student@e450-8 <student@e450-8.res.training>
2018-06-01 06:17:53 +0200

    select-socket

--- altio/message.h
+++ altio/message.h
@@ -57,3 +57,82 @@ void delete_message_queue(message_queue_t *queue)
     free(queue->data);
     queue->data = NULL;
 }
+
+int enqueue(message_queue_t *queue, message_t *message) {
+    if (queue->current == queue->size)
+        return -1;
+
+    memcpy(&queue->data[queue->current], message, sizeof(message_t));
+    queue->current++;
+
+    return 0;
+} 
+
+int dequeue(message_queue_t *queue, message_t *message) {
+    if (queue->current == 0)
+        return -1;
+    
+    memcpy(message, &queue->data[&queue->current-1], sizeof(message_t));
+    queue->current--;
+
+    return 0;
+}
+
+int dequeue_all(message_queue_t *queue) {
+    queue->current=0;
+    return 0;
+}
+
+// peer -------------------------------------------------------------------------------------------------------------------------------------
+typedef struct {
+    int socket;
+    struct sockaddr_in addr;
+
+    /* Messages waiting for send */
+    message_queue_t send_buffer;
+
+    /*  Buffered sending message
+     *
+     *  In case we don't send the whole message per one call send()
+     *  And current_sending_byte is a pointer to the part of data that will be send next call.
+     * */
+    message_t sending_buffer;
+    size_t current_sending_byte;
+
+    /* The same for the receiving message. */
+    message_t receiving_buffer;
+    size_t current_receiving_byte;
+} peer_t;
+
+int delete_peer(peer_t *peer) {
+    close(peer->socket);
+    delete_message_queue(&peer->send_buffer);
+}
+
+int create_peer(peer_t *peer) {
+    create_message_queue(MAX_MESSAGES_BUFFER_SIZE, &peer->send_buffer);
+
+    peer->current_sending_byte = -1;
+    peer->current_receiving_byte = 0;
+
+    return 0;
+}
+
+char *peer_get_address_str(peer_t *peer) {
+    static char ret[INET_ADDRSTRLEN + 10];
+    char peer_ipv4_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &peer->addr.sin_addr, peer_ipv4_str, INET_ADDRSTRLEN);
+    sprintf(ret, "%s:%d\n", peer_ipv4_str, peer->addr.sin_port);
+
+    return ret;
+}
+
+int peer_add_to_send(peer_t *peer, message_t *message) {
+    return enqueue(&peer->send_buffer, message);
+}
+
+/* Receive message from peer and handle it with the message_handler() */
+int receive_from_peer(peer_t *peer, int (*message_handler)(message_t))
+{
+    printf("Ready for recv() from %s.\n", peer_getaddress_str(peer));
+}
