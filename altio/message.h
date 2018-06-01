abd6a30993216100ae74cae391907fb2e7ab43f1
Miltos69 <mvimblis@gmail.com>
2018-06-02 01:01:01 +0300

    all code ready

--- altio/message.h
+++ altio/message.h
@@ -135,4 +135,148 @@ int peer_add_to_send(peer_t *peer, message_t *message) {
 int receive_from_peer(peer_t *peer, int (*message_handler)(message_t))
 {
     printf("Ready for recv() from %s.\n", peer_getaddress_str(peer));
+
+    size_t len_to_receive;
+    ssize_t received_count;
+    size_t received_total = 0;
+    do {
+        // is completely received
+        if (peer->current_receiving_byte >= sizeof(peer->receiving_buffer)) {
+            message_handler(&peer->receiving_buffer);
+            peer->current_receiving_byte = 0;
+        }
+
+        // Count bytes to send
+        len_to_receive = sizeof(peer->receiving_buffer) - peer->current_receiving_byte;
+        if (len_to_receive > MAX_SEND_SIZE)
+            len_to_receive = MAX_SEND_SIZE;
+
+        printf("Let's try to recv() %zd bytes ...", len_to_receive);
+        received_count = recv(peer->socket, (char *)&peer->receiving_buffer + peer->current_receiving_byte,
+            len_to_receive, MSG_NONTWAIT);
+        if (received_count < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                printf("peer is not ready right now, try again later.\n");
+            }
+            else {
+                perror("recv() from peer error");
+                return -1;
+            }
+        }
+        else if (received_count < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            break;
+        }
+        // If recv() returns 0, it means that peer gracefully shutdown. Shutdown client
+        else if (received_count == 0) {
+            printf("recv() 0 bytes. Peer gracefully shutdown.\n");
+            return -1;
+        }
+        else if (received_count > 0) {
+            peer->current_receiving_byte += received_count;
+            received_total += received_count;
+
+            printf("recv() %zd bytes.\n", received_count);
+        }
+    } while (received_count > 0);
+
+    printf("Total recv()'ed %zd bytes.\n", received_total);
+
+    return 0;
 }
+
+int send_to_peer(peer_t *peer) {
+    printf("Ready to send() to %s.\n", peer_get_address_str(peer));
+
+    size_t len_to_send;
+    ssize_t send_count;
+    size_t send_total = 0;
+
+    do {
+        // If sending message has completely sent and there messages into queue, why not send them ?
+        if (peer->current_sending_byte < 0 || peer->current_sending_byte >= sizeof(peer->sending_buffer)) {
+            printf("There is no pending to send() message, maybe we can find one in queue ...");
+            if (dequeue(&peer->send_buffer, &peer->sending_buffer) != 0 ) {
+                peer->current_sending_byte = -1;
+                printf("No, there is nothing to send() anymore.\n");
+                break;
+            }
+
+            printf("Yes, pop and send() one of them.\n");
+            peer->current_sending_byte = 0;
+        }
+
+        // Count bytes to send
+        len_to_send = sizeof(peer->sending_buffer) - peer->current_sending_byte;
+        if (len_to_send > MAX_SEND_SIZE)
+            len_to_send = MAX_SEND_SIZE;
+
+        printf("Let's try to send() %zd bytes ...", len_to_send);
+        send_count = send(peer->socket, (char *)&peer->sending_buffer + peer->current_sending_byte, len_to_send, 0);
+        if (send_count < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                printf("peer is not ready right now, try again later.\n");
+            } 
+            else {
+                perror("send() from peer eror");
+                return -1;
+            }
+        }
+        // we have read as many as possible
+        else if (send_count < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            break;
+        }
+        else if (send_count == 0) {
+            printf("send()'ed 0 bytes. It seems that peer can't accept data right now. Try again later.\n");
+            break;
+        }
+        else if (send_count > 0) {
+            peer->current_sending_byte += send_count;
+            send_total += send_count;
+            printf("send()'ed %zd bytes.\n", send_count);
+        }
+    } while (send_count > 0);
+
+    printf("Total send()'ed %zu bytes.\n", send_total);
+
+    return 0;
+}
+
+// common ------------------------------------------------------------------------
+
+/* Reads from stdin and create new message. This message enqueues to send queue. */
+int read_from_stdin(char *read_buffer, size_t max_length) {
+    memset(read_buffer, 0, max_length);
+
+    ssize_t read_count = 0;
+    ssize_t total_read = 0;
+
+    do {
+        read_count = read(STDIN_FILENO, read_buffer, max_len);
+        if (read_count < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+            perror("read()");
+            return -1;
+        }
+        else if (read_count < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            break;
+        }
+        else if (read_count > 0) {
+            total_read += read_count;
+            if (total_read > max_len) {
+                printf("Message too large and will be chopped. Please try to be storter next time.\n");
+                fflush(STDIN_FILENO);
+                break;
+            }
+        }
+    } while (read_count > 0);
+
+    size_t len = strlen(read_buffer);
+    if (len > 0 && read_buffer[len-1] == '\n') {
+        read_buffer[len-1] = '\0';
+    }
+
+    printf("Read from stdin %zu bytes. Let's prepare message to send.\n", strlen(read_buffer));
+
+    return 0;
+}
+
+#endif /* MESSAGE_H */
--- altio/server.c
+++ altio/server.c
@@ -1,4 +1,56 @@
+// Simple example of a server with select() 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>
+#include <netinet.h>
+#include <unistd.h>
+
+#include "message.h"
+
+#define MAX_CLIENTS 10
+
+#define NO_SOCKET -1
+
+#define SERVER_NAME "server"
+
 int listen_sock;
+peer_t connection_list[MAX_CLIENTS];
+char read_buffer[1024];         // buffer for stdin
+
+void shutdown_properly(int code);
+
+void handle_signal_action(int sig_number) {
+    if (sig_number == SIGINT) {
+        printf("SIGINT was catched!\n");
+        shutdown_properly(EXIT_SUCCESS);
+    }
+    else if (sig_number == SIGPIPE) {
+        printf("SIGPIPE was catched!\n");
+        shutdown_properly(EXIT_SUCCESS);
+    }
+}
+
+int setup_signals() {
+    struct sigaction sa;
+
+    sa.sa_handler = handler_signal_action;
+
+    if (sigaction(SIGINT, &sa, 0) != 0) {
+        perror("sigaction()");
+        return -1;
+    }
+    if (sigaction(SIGPIPE, &sa, 0) != 0) {
+        perror("sigaction()");
+        return -1;
+    }
+
+    return 0;
+}
 
 /* Start listening socket "listen_sock" */
 int 
@@ -38,6 +90,19 @@ start_listen_socket(int *listen_sock) {
     return 0;
 }
 
+void shutdown_properly(int code) {
+    int i;
+
+    for (i=0; i<MAX_CLIENTS; i++) {
+        if (connections_list[i].socket != NO_SOCKET) {
+            close(connections_list[i].socket)
+        }
+    }
+
+    printf("Shutdown server properly.\n");
+    exit(code);
+}
+
 int 
 build_fd_sets(fd_set &read_fds, fd_set &write_fds)
 {
@@ -46,6 +111,209 @@ build_fd_sets(fd_set &read_fds, fd_set &write_fds)
     FD_ZERO(read_fds);
     FD_SET(STDIN_FILENO, read_fds);
     FD_SET(listen_socket, read_fds);
+    for (i=0; i<MAX_CLIENTS; i++) {
+        if (connection_list[i].socket != NO_SOCKET) {
+            FD_SET(connection_list[i].socket, read_fds);
+        }
+    }
 
     FD_ZERO(write_fds);
+    for (i=0; i<MAX_CLIENTS; i++) {
+        if (connection_list[i].socket != NO_SOCKET && connection_list[i].send_buffer.current > 0) {
+            FD_SET(connection_list[i].socket, write_fds);
+        }
+    }
+
+    FD_ZERO(except_fds);
+    FD_SET(STDIN_FILENO, except_fds);
+    FD_SET(listen_sock, except_fds);
+    for (i=0; i<MAX_CLIENTS; i++) {
+        if (connection_list[i].socket != NO_SOCKET) {
+            FD_SET(connection_list[i].socket, except_fds);
+        }
+    }
+
+    return 0;
+}
+
+int handle_new_connection() {
+    struct sockaddr_in client_addr;
+    memset(&client_addr, 0, sizeof(client_addr));
+    socklen_t client_len = sizeof(client_addr);
+    
+    int new_client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_len);
+
+    if (new_client_sock < 0) {
+        perror("accept()");
+        return -1;
+    }
+
+    char client_ipv4_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ipv4_str, INET_ADDRSTRLEN);
+
+    printf("Incoming connection from %s:%d.\n", client_ipv4_str, client_add.sin_port);
+
+    int i;
+    for (i=0; i<MAX_CLIENTS; i++) {
+        if (connection_list[i].socket == NO_SOCKET) {
+            connection_list[i].socket = new_client_sock;
+            connection_list[i].addr = client_addr;
+            connection_list[i].current_sending_byte = -1;
+            connection_list[i].current_receiving_byte = 0;
+
+            return 0;
+        }
+    }
+
+    printf("There are too many connection. Close new connection %s:%d.\n", client_ipv4_str, client_addr.sin_port);
+
+    close(new_client_sock);
+
+    return -1;
+}
+
+int close_client_connection(peer_t *client) {
+    printf("Close client socket for %s.\n", peer_get_address_str(client));
+    
+    close(client->socket);
+    client->socket = NO_SOCKET;
+
+    dequeue_all(&client->send_buffer);
+
+    client->current_reading_byte = -1;
+    client->current_receiving_byte = 0;
+}
+
+/* Reads from stdin and create new message. This message enqueues to send queueu. */
+int handle_read_from_stdin() {
+    char read_buffer[DATA_MAXSIZE];
+
+    if (read_from_stdin(read_buffer, DATA_MAXSIZE) != 0) {
+        perror("read_from_stdin()");
+        return -1;
+    }
+
+    // Create new messages and enqueue it.
+    message_t new_message;
+    prepare_message(SERVER_NAME, read_buffer, &new_message);
+    print_message(&new_message);
+
+    // Enqueue message for all clients
+    int i;
+    for (i=0; i<MAX_CLIENTS; i++) {
+        if (connection_list[i].socket != NO_SOCKET) {
+            if (peer_add_to_send(&connection_list[i], &new_message) != 0) {
+                printf("Send buffer was overflowed, we lost this message!\n");
+                continue;
+            }
+            printf("New message to send was enqueued right now!\n");
+        }
+    }
+
+    return 0;
+}
+
+int handle_received_message(message_t *message) {
+    printf("Received message from client.\n");
+    print_message(message);
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if (setup_signals() != 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (start_listen_socket(&listen_sock) != 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Set nonblock for stdin. */
+    int flag = fcntl(STDIN_FILENO, F_GETFL, 0);
+    flag |= O_NONBLOCK;
+    fcntl(STDIN_FILENO, F_SETFL, flag);
+
+    int i;
+    for (i=0; i<MAX_CLIENTS; i++) {
+        connection_list[i].socket = NO_SOCKET;
+        create_peer(&connection_list[i]);
+    } 
+
+    fd_set read_fds;
+    fd_set write_fds;
+    fd_ser except_fds;
+
+    int high_sock = listen_sock;
+
+    printf("Waiting for incomming connections.\n");
+
+    while (1) {
+        build_fd_sets(&read_fds, &write_fds, &except_fds, NULL);
+
+        high_sock = listen_sock;
+
+        for (i=0; i<MAX_CLIENTS; i++) {
+            if (connection_list[i].socket > high_sock) {
+                high_sock = connection_list[i].socket;
+            }
+        }
+
+        int activity = select(high_sock+1, &read_fds, &write_fds, &except_fds, NULL);
+
+        switch (activity) {
+            case -1:
+                perror("select");
+                shutdown_properly(EXIT_FAILURE);
+            case 0:
+                printf("select() return 0.\n");
+                shutdown_properly(EXIT_FAILURE);
+            default:
+                // All set fds should be checked
+                if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+                    if (handle_read_from_stdin() != 0) {
+                        shutdown_properly(EXIT_FAILURE);
+                    }
+                }
+
+                if (FD_ISSET(listen_sock, &read_fds)) {
+                    handle_new_connection();
+                }
+
+                if (FD_ISSET(STDIN_FILENO, &except_fds)) {
+                    printf("except fds for stdin.\n");
+                    shutdown_properly("EXIT_FAILURE");
+                }
+
+                if (FD_ISSET(listen_sock, &except_fds)) {
+                    printf("Exception listen socket fd.\n");
+                    shutdown_properly("EXIT_FAILURE");
+                }
+
+                for (i=0; i<MAX_CLIENTS; i++) {
+                    if  (connection_list[i].socket != NO_SOCKET && FD_ISSET(connection_list[i].socket, &read_fds)) {
+                        if (receive_from_peer(&connection_list[i], &handle_received_message) != 0) {
+                            close_client_connection(&connection_list[i]);
+                            continue;
+                        }
+                    }
+
+                    if (connection_list[i].socket != NO_SOCKET && FD_ISSET(connection_list[i].socket, &write_fds)) {
+                        if (send_to_peer(&connection_list[i]) != 0) {
+                            close_client_connection(&connection_list[i]);
+                            continue;
+                        }
+                    }
+
+                    if (connection_list[i].socket != NO_SOCKET && FD_ISSET(connection_list[i].socket, &except_fds)) {
+                        printf("Exception client fd.\n");
+                        close_client_connection(&connection_list[i]);
+                        continue;
+                    }
+                }
+        }
+        printf("And we are still waiting for clients or stdin activity");
+    }
+
+    return 0;
 }
