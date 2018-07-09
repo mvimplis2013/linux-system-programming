6b7a7b9547b2830ea58f90f89a233d174c185c3c
student@e450-8 <student@e450-8.res.training>
2018-07-09 19:30:43 +0200

    args_parse

--- mqtt/my_command_line_parser.h
+++ mqtt/my_command_line_parser.h
@@ -3,20 +3,21 @@
 
 /* Flag set by '--verbose' */
 static int verbose_flag;
- 
-static struct option long_options[] = {
-            /* These options set a flag */
-            {"verbose", no_argument, &verbose_flag, 1},
-            {"brief",   no_argument, &verbose_flag, 0},
-            /* These options do not set a flag. We distinguish them by their indices */
-            {"add", no_argument, 0, 'a'},
-            {"append", no_argument, 0, 'b'},
-            {"delete", required_argument, 0, 'd'},
-            {"create", required_argument, 0, 'c'},
-            {"file",   required_argument, 0, 'f'},
-            {0, 0, 0, 0}
-        };
-int
+
+static const char *optstring = "t:dev:";
+
+static const struct option long_options_button[] = {
+    /* These options set a flag */
+    {"verbose", no_argument, &verbose_flag, 1},
+    {"brief",   no_argument, &verbose_flag, 0},
+    
+    /* These options do not set a flag. We distinguish them by their indices */
+    {"topic", required_argument, 0, 't'},
+    {"device", required_argument, 0, 'dev'},
+    {0, 0, 0, 0}
+};
+
+int 
 argsparse_button(int argc, char **argv);
 
 #endif
