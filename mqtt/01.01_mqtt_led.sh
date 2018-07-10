df4cd3d263dd8b458b503dc40647bbcda38b2835
student@e450-8 <student@e450-8.res.training>
2018-07-10 09:24:35 +0200

    mqtt_led

---
+++ mqtt/01.01_mqtt_led.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source ../../../env.sh
+source local-config-led.mak
+
+echo "+ make clean"
+make -f Makefile_led clean
+
+if [[ $1 != "DONT_WAIT" ]]; then
+  vim $PROJECT_NAME.c
+  echo "+ build $PROJECT_NAME"
+  echo "+ press <ENTER> to go on"
+  read r
+fi
+
+echo "+ make <Makefile>"
+make -f Makefile_led
+
+if [[ $DEVKIT = "native" ]]; then
+  echo "+ execute $PROJECT_NAME"
+  echo "+ press <ENTER> to go on"
+  read r
+  ./$PROJECT_NAME.out
+fi
---
+++ mqtt/local-config-led.mak
@@ -0,0 +1,1 @@
+PROJECT_NAME=mqtt_led
